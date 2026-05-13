ec23cfab2f4872f784f797ac520e2eac2845eaff
mknnnnnnn <maksymilian.gerlach@gmail.com>
2026-05-13 15:26:46 +0200

    Add database connection and example env file

---
+++ src/shrink_it/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy import create_engine
+
+from .config import DATABASE_URL
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
