a19a2315a22f1c7eef35130d06753b2e3fa20e4a
mknnnnnnn <maksymilian.gerlach@gmail.com>
2026-05-15 11:33:47 +0200

    Add endpoints for URL retrieval, activation and deactivation

--- src/shrink_it/urls/router.py
+++ src/shrink_it/urls/router.py
@@ -1,6 +1,12 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from .service import url_create as url_create_service
+from .service import (
+    url_create as url_create_service,
+    url_get_by_short_code as url_get_by_short_code_service,
+    url_get as url_get_service,
+    url_deactivate as url_deactivate_service,
+    url_activate as url_activate_service,
+)
 from .schemas import *
 from ..database import get_db
 
@@ -10,3 +16,23 @@ router = APIRouter(tags=["urls"], prefix="/urls")
 @router.post("/create")
 def create_url(url: URLCreate, db: Session = Depends(get_db)):
     return url_create_service(url=url, db=db)
+
+
+@router.get("/get", response_model=list[URLResponse])
+def get_url(db: Session = Depends(get_db)):
+    return url_get_service(db=db)
+
+
+@router.get("/{short_code}", response_model=URLResponse)
+def get_url_by_short_code(shore_code: str, db: Session = Depends(get_db)):
+    return url_get_by_short_code_service(short_code=shore_code, db=db)
+
+
+@router.patch("/deactivate/{id}", response_model=URLResponse)
+def url_deactivate(id: int, db: Session = Depends(get_db)):
+    return url_deactivate_service(id=id, db=db)
+
+
+@router.patch("/activate/{id}", response_model=URLResponse)
+def url_activate(id: int, db: Session = Depends(get_db)):
+    return url_activate_service(id=id, db=db)
--- src/shrink_it/urls/service.py
+++ src/shrink_it/urls/service.py
@@ -1,7 +1,9 @@
+from fastapi import HTTPException
 from random import choices
 import string
 
 from sqlalchemy.orm import Session
+from sqlalchemy import select
 
 from .schemas import URLCreate
 from .models import URL
@@ -9,6 +11,11 @@ from .models import URL
 
 def generate_code(length: int = 5) -> str:
     return "".join(choices(string.ascii_letters + string.digits, k=length))
+
+
+def increase_click_count(url: URL, db: Session):
+    url.click_count += 1
+    db.commit()
 
 
 def url_create(url: URLCreate, db: Session):
@@ -26,3 +33,67 @@ def url_create(url: URLCreate, db: Session):
     db.refresh(db_url)
 
     return db_url
+
+
+def url_get(db: Session):
+    statement = select(URL)
+    return db.scalars(statement).all()
+
+
+def url_get_by_short_code(short_code: str, db: Session):
+    statement = select(URL).where(URL.short_code == short_code)
+    db_url = db.scalar(statement)
+
+    if db_url is None:
+        raise HTTPException(status_code=404, detail="URL NOT FOUND")
+
+    if db_url.is_active is False:
+        raise HTTPException(status_code=400, detail="URL INACTIVE")
+
+    if db_url.click_max is not None and db_url.click_count >= db_url.click_max:
+        raise HTTPException(status_code=403, detail="URL click limit has benn reached")
+
+    increase_click_count(db_url, db)
+
+    return db_url
+
+
+def url_deactivate(id: int, db: Session):
+    statement = select(URL).where(URL.id == id)
+    db_url = db.scalar(statement)
+
+    if db_url is None:
+        raise HTTPException(status_code=404, detail="URL NOT FOUND")
+
+    if db_url.is_active is False:
+        raise HTTPException(status_code=400, detail="URL INACTIVE")
+
+    db_url.is_active = False
+    db.commit()
+    db.refresh(db_url)
+
+    return db_url
+
+
+def url_activate(id: int, db: Session):
+    statement = select(URL).where(URL.id == id)
+    db_url = db.scalar(statement)
+
+    if db_url is None:
+        raise HTTPException(status_code=404, detail="URL NOT FOUND")
+
+    if db_url.is_active is True:
+        raise HTTPException(status_code=400, detail="URL ACTIVE")
+
+    db_url.is_active = True
+    db.commit()
+    db.refresh(db_url)
+
+    return db_url
+
+
+# Change click max
+
+# Change expire date
+
+# Delete url
