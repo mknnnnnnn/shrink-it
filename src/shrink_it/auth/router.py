07ecef4159c8c04db328e81e52086673783bdde2
mknnnnnnn <maksymilian.gerlach@gmail.com>
2026-05-24 17:11:42 +0200

    Add auth register and login routes

--- src/shrink_it/auth/router.py
+++ src/shrink_it/auth/router.py
@@ -1,6 +1,12 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from ..users.schemas import UserRegister, UserLogin, UserResponse
+from .service import (
+    login_user as login_user_service,
+    register_user as register_user_service,
+)
+from .schemas import Token
+from .dependencies import get_current_user
 from ..database import get_db
 
 router = APIRouter(prefix="/auth", tags=["auth"])
@@ -8,9 +14,9 @@ router = APIRouter(prefix="/auth", tags=["auth"])
 
 @router.post("/register", response_model=UserResponse)
 def register_user(user: UserRegister, db: Session = Depends(get_db)):
-    pass
+    return register_user_service(user=user, db=db)
 
 
-@router.post("/login")
+@router.post("/login", response_model=Token)
 def login_user(user: UserLogin, db: Session = Depends(get_db)):
-    pass
+    return login_user_service(user=user, db=db)
