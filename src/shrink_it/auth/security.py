1d7b37b7dec3c9d21edcc3a2f81b4247fff39008
mknnnnnnn <maksymilian.gerlach@gmail.com>
2026-05-24 16:23:33 +0200

    Add token decoding

--- src/shrink_it/auth/security.py
+++ src/shrink_it/auth/security.py
@@ -1,7 +1,8 @@
+from ..config import ALGORITHM, SECRET, ACCESS_TOKEN_EXPIRE
 from passlib.context import CryptContext
 from datetime import datetime, timedelta, timezone
-from ..config import ALGORITHM, SECRET, ACCESS_TOKEN_EXPIRE
-from jose import jwt
+from jose import jwt, JWTError
+from fastapi import HTTPException
 
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -18,7 +19,13 @@ def create_token(data: dict):
 
     to_encode.update({"exp": expire})
 
-    encoded_jwt = jwt.encode(to_encode, SECRET, ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, SECRET, algorithm=[ALGORITHM])
 
     return encoded_jwt
 
+def decode_token(token: str):
+    try:
+        decoded_jwt = jwt.decode(token, SECRET, algorithms=[ALGORITHM])
+        return decoded_jwt
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
