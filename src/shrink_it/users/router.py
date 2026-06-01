a96307100e74c510fd5889f4628613be1970bfb1
mknnnnnnn <maksymilian.gerlach@gmail.com>
2026-06-01 18:58:05 +0200

    Add user status management endpoints

--- src/shrink_it/users/router.py
+++ src/shrink_it/users/router.py
@@ -3,27 +3,37 @@ from .schemas import UserResponse
 from sqlalchemy.orm import Session
 from ..database import get_db
 from ..auth.dependencies import require_admin
-from .service import (
-    user_get as user_get_service,
-    user_get_by_id as user_get_by_id_service,
-    user_delete as user_delete_service,
-)
+from . import service
 
-router = APIRouter(tags=["me"], prefix="/me")
+router = APIRouter(tags=["users"], prefix="/users")
 
 # Only admin endpoints
 
 
-@router.get("/get", response_model=list[UserResponse])
+@router.get("", response_model=list[UserResponse])
 def users_get(db: Session = Depends(get_db), user=Depends(require_admin)):
-    return user_get_service(db=db)
+    return service.user_get(db=db)
 
 
-@router.get("/get/{id}", response_model=UserResponse | None)
+@router.get("{id}", response_model=UserResponse | None)
 def user_get_by_id(id: int, db: Session = Depends(get_db), user=Depends(require_admin)):
-    return user_get_by_id_service(id=id, db=db)
+    return service.user_get_by_id(id=id, db=db)
 
 
-@router.delete("/delete/{id}")
+@router.delete("/{id}/delete")
 def user_delete(id: int, db: Session = Depends(get_db), user=Depends(require_admin)):
-    return user_delete_service(id=id, db=db)
+    return service.user_delete(id=id, db=db)
+
+
+@router.patch("/{id}/active-status/{status}")
+def user_active_status(
+    id: int, status: bool, db: Session = Depends(get_db), user=Depends(require_admin)
+):
+    return service.user_active_status(id=id, active_status=status, db=db)
+
+
+@router.patch("/{id}/admin-status/{status}")
+def user_admin_status(
+    id: int, status: bool, db: Session = Depends(get_db), user=Depends(require_admin)
+):
+    return service.user_admin_status(id=id, admin_status=status, db=db)
--- src/shrink_it/users/service.py
+++ src/shrink_it/users/service.py
@@ -26,7 +26,38 @@ def user_delete(id: int, db: Session):
     if db_user is None:
         raise HTTPException(status_code=404, detail="USER NOT FOUND")
 
+    if db_user.is_admin:
+        raise HTTPException(status_code=403, detail="FORBIDDEN")
+
     db.delete(db_user)
     db.commit()
 
+    return {"message": "User has been deleted"}
+
+
+def user_active_status(id: int, active_status: bool, db: Session):
+    db_user = db.get(User, id)
+
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="USER NOT FOUND")
+
+    db_user.is_active = active_status
+    db.commit()
+
     return {"message": "success"}
+
+
+def user_admin_status(id: int, admin_status: bool, db: Session):
+    user_db = db.get(User, id)
+
+    if user_db is None:
+        raise HTTPException(status_code=404, detail="USER NOT FOUND")
+
+    user_db.is_admin = admin_status
+    db.commit()
+
+    return {"message": "success"}
+
+
+def show_urls(id: int, db: Session):
+    pass
