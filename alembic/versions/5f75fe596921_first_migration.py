0e34a7d8c2330a8f39a9478901aa368239a3eab2
mknnnnnnn <maksymilian.gerlach@gmail.com>
2026-07-31 18:18:47 +0200

    New first migration

--- alembic/versions/5f75fe596921_first_migration.py
+++ alembic/versions/5f75fe596921_first_migration.py
@@ -1,8 +1,8 @@
 """First migration
 
-Revision ID: c7f2499dd81c
+Revision ID: 5f75fe596921
 Revises:
-Create Date: 2026-06-14 14:56:16.007721
+Create Date: 2026-07-31 16:13:34.099406
 
 """
 
@@ -12,7 +12,7 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision: str = "c7f2499dd81c"
+revision: str = "5f75fe596921"
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -24,8 +24,8 @@ def upgrade() -> None:
     op.create_table(
         "users",
         sa.Column("id", sa.Integer(), nullable=False),
-        sa.Column("first_name", sa.String(), nullable=False),
-        sa.Column("last_name", sa.String(), nullable=False),
+        sa.Column("first_name", sa.String(length=20), nullable=False),
+        sa.Column("last_name", sa.String(length=20), nullable=False),
         sa.Column("email", sa.String(), nullable=False),
         sa.Column("password", sa.String(), nullable=False),
         sa.Column("phone_number", sa.String(), nullable=True),
@@ -45,10 +45,10 @@ def upgrade() -> None:
         "urls",
         sa.Column("id", sa.Integer(), nullable=False),
         sa.Column("original_url", sa.String(), nullable=False),
-        sa.Column("short_code", sa.String(), nullable=False),
+        sa.Column("short_code", sa.String(length=10), nullable=False),
         sa.Column("created_at", sa.DateTime(), nullable=False),
         sa.Column("expires_at", sa.DateTime(), nullable=True),
-        sa.Column("click_count", sa.Integer(), nullable=True),
+        sa.Column("click_count", sa.Integer(), nullable=False),
         sa.Column("click_max", sa.Integer(), nullable=True),
         sa.Column("is_active", sa.Boolean(), nullable=False),
         sa.Column("user_id", sa.Integer(), nullable=False),
