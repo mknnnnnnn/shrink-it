fc55f7cf96192ef9ff1c6276419ac019b6df6acc
mknnnnnnn <maksymilian.gerlach@gmail.com>
2026-08-01 16:20:01 +0200

    Remove unused imports

--- src/shrink_it/auth/router.py
+++ src/shrink_it/auth/router.py
@@ -1,19 +1,21 @@
 from fastapi import APIRouter, Depends, status
-from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordRequestForm
-from ..users.schemas import UserRegister, UserLogin, UserResponse
+from sqlalchemy.orm import Session
+
+from ..users.schemas import UserRegister, UserResponse
+from ..database import get_db
 from .service import (
     login_user as login_user_service,
     register_user as register_user_service,
 )
 from .schemas import Token
-from .dependencies import get_current_user
-from ..database import get_db
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 
-@router.post("/register", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+@router.post(
+    "/register", response_model=UserResponse, status_code=status.HTTP_201_CREATED
+)
 def register_user(user: UserRegister, db: Session = Depends(get_db)):
     return register_user_service(user=user, db=db)
 
